b576994cdecc102efc2049420973afccefcf51e3
henryhrcao <henryhrcao@gmail.com>
2025-07-14 11:36:59 -0400

    Starter

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+from constants import *
+def main():
+    print("Starting Asteroids!")
+    print("Screen width: " + str(SCREEN_WIDTH))
+    print("Screen height: " + str(SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, (0,0,0))
+        pygame.display.flip()
+
+
+
+if __name__ == "__main__":
+    main()
